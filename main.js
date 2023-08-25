8b7a1fbff1f68b43d125d4dc5beeed3e5b79ccba
Pixel <132843621+thepixelist11@users.noreply.github.com>
2023-08-25 12:30:37 -0400

    Attempt to fix an issue

--- main.js
+++ main.js
@@ -304,3 +304,29 @@ function getOdometer(token){
   .then(response => console.log(response))
   .catch(err => console.error(err));
 }
+
+var loadJSONP = (function(){
+  var unique = 0;
+  return function(url, callback, context) {
+    // INIT
+    var name = "_jsonp_" + unique++;
+    if (url.match(/\?/)) url += "&callback="+name;
+    else url += "?callback="+name;
+    
+    // Create script
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    
+    // Setup handler
+    window[name] = function(data){
+      callback.call((context || window), data);
+      document.getElementsByTagName('head')[0].removeChild(script);
+      script = null;
+      delete window[name];
+    };
+    
+    // Load JSON
+    document.getElementsByTagName('head')[0].appendChild(script);
+  };
+})();
